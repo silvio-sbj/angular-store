a1c0659a301fc39b3ed92e009fe1a2b54280f610
Silvio Junior <silvio_sbj@hotmail.com>
2024-12-26 16:54:33 -0400

    Files added via upload

---
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pricing',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-pricing.component.html',
+  styleUrl: './card-pricing.component.css'
+})
+export class CardPricingComponent {
+
+  @Input()
+  gameType: string = '';
+
+  @Input()
+  gamePrice: string = '';
+
+}
